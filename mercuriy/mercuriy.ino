6be2911e99e84e100d42266485f7536e54c7d382
valeraba <baturinva@yandex.ru>
2017-10-25 11:00:25 +0700

    v

--- mercuriy/mercuriy.ino
+++ mercuriy/mercuriy.ino
@@ -514,7 +514,7 @@ bool run_ds(TimeStamp time)
         divider = 16;
       else
         Serial.print("Sensor is error type.");
-      float temp = (ram[1] << 8) + ram[0];
+      float temp = (__int16)((ram[1] << 8) | ram[0]);
       tempCels[i] = temp / divider;
     }
     else
